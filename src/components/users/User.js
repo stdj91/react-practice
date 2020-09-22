f162d2b8e287dd30bfe8bb0360c035a310cbb31b
stdj91 <stdj.1907@gmail.com>
2020-09-22 13:54:46 +0200

    added to user card: company, login, website

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -9,6 +9,7 @@ export class User extends Component {
     render() {
         const {
             name,
+            company,
             avatar_url,
             location,
             bio,
@@ -33,6 +34,35 @@ export class User extends Component {
                         <img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
                         <h1>{name}</h1>
                         <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (
+                            <Fragment>
+                                <h3>Bio</h3>
+                                <p>{bio}</p>
+                            </Fragment>
+                        )}
+                        <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+                        <ul>
+                            <li>
+                                {login && <Fragment>
+                                <strong>Username: </strong> {login}
+                                </Fragment>}
+                                
+                            </li>
+                            <li>
+                                {company && <Fragment>
+                                <strong>Company: </strong> {company}
+                                </Fragment>}
+                                
+                            </li>
+                            <li>
+                                {blog && <Fragment>
+                                <strong>Website: </strong> {blog}
+                                </Fragment>}
+                                
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </Fragment>
