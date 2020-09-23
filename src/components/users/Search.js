6b4e21b7f604992a38f3cd16d92cabe8d8a3dcbb
stdj91 <stdj.1907@gmail.com>
2020-09-23 17:40:00 +0200

    Search comp changed to func comp

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Search extends Component {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+    const [text, setText] = useState('');
 
-    state = {
-        text: ''
-    }
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter something...', 'light')
+        if (text === '') {
+            setAlert('Please enter something...', 'light')
         } else {
 
-            this.props.searchUsers(this.state.text);
-            this.setState({ text: '' })
+            searchUsers(text);
+            setText('');
         }
     }
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
+    const onChange = (e) => {
+        setText(e.target.value)
     }
 
-    render() {
+    
         return (
             <div>
-                <form onSubmit={this.onSubmit} className='form'>
-                    <input type='text' name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange} />
+                <form onSubmit={onSubmit} className='form'>
+                    <input type='text' name='text' placeholder='Search Users...' value={text} onChange={onChange} />
                     <input type='submit' value='Search' className='btn btn-dark btn-block' />
                 </form>
-                {this.props.showClear && 
-                <button className='btn btn-light btn-block' onClick={this.props.clearUsers} >Clear</button>}
+                {showClear && 
+                <button className='btn btn-light btn-block' onClick={clearUsers} >Clear</button>}
                 
             </div>
         )
-    }
 }
 
 export default Search
